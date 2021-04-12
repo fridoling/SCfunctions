840a0e216ea21b5e8cf419686ec9b0dbc5bef9bf
fridoling <fridoling@hotmail.com>
2021-04-12 12:25:15 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='SCfunctions',
+    version='0.1.0',
+    author='Fridolin Gross',
+    author_email='fridoling@hotmail.com',
+    packages=['SCfunctions']
+    scripts=['SCfunctions.py']
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    description='Custom functions for use with SloppyCell',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
